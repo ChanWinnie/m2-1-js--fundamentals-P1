cc4f4b2fcad686f52f88e5a3a9baac201c00e96e
“Winnie <chanwinnie.wc@gmail.com>
2020-09-22 12:17:49 -0400

    Exercise 9 completed

--- __workshop/exercise-9.js
+++ __workshop/exercise-9.js
@@ -28,6 +28,22 @@ function exercise9() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  let n = 1;
+
+  while (n <= 100) { 
+    
+    if (n % 3 === 0 && n % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (n % 5 === 0) {
+      console.log("Buzz");
+    } else if (n % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(n);
+    }
+    n ++;
+  }
+  
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
